95f299e13f8190bcace6038f0d7f87874a59ce2a
raju <jtt.raju@gmail.com>
2018-11-30 11:46:26 +0530

    Initial Commit

---
+++ AcquireSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcquireSDK
+//
+//  Created by Raju Jangid on 11/30/18.
+//  Copyright © 2018 AcquireIO Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcquireSDK/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  AcquireSDK
+//
+//  Created by Raju Jangid on 11/30/18.
+//  Copyright © 2018 AcquireIO Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
